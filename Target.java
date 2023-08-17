d1dc8fd3f10787d13b4fc93a03b33f058823bf3c
Rohit Sai Anjan.D <119723402+Rohit-05002@users.noreply.github.com>
2023-08-17 11:10:03 +0530

    import java.util.*; public class we{     public static void main(String[]args){         Scanner sc=new Scanner(System.in);         int p1=sc.nextInt();         int p2=sc.nextInt();         int p3=sc.nextInt();         int p4=sc.nextInt();         if (p1>=10 && p2>=10 && p3>=10 && p4>=10){             System.out.print("YES");         }         else{             System.out.print("NO");         }
        }
    }

---
+++ Target.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class we{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int p1=sc.nextInt();
+        int p2=sc.nextInt();
+        int p3=sc.nextInt();
+        int p4=sc.nextInt();
+        if (p1>=10 && p2>=10 && p3>=10 && p4>=10){
+            System.out.print("YES");
+        }
+        else{
+            System.out.print("NO");
+        }
+        
+    }
+}
