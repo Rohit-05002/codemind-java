2adc6a9a80ecb50ca8707958ae7288f8138063f5
Rohit Sai Anjan.D <119723402+Rohit-05002@users.noreply.github.com>
2023-08-18 11:53:40 +0530

    import java.util.*; public class num {     public static void main(String[] args)     {         int i,n;         Scanner sc = new Scanner(System.in);         n = sc.nextInt();         for (i=3;i<=3*n;i++){         if (i%3==0)         {         System.out.print(i +" ");         }
            }
            
        }
    }

---
+++ First_N_multiples_of_3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class num
+{
+    public static void main(String[] args)
+    {
+        int i,n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (i=3;i<=3*n;i++){
+        if (i%3==0)
+        {
+        System.out.print(i +" ");
+        }
+            
+        }
+        
+    }
+}
