468828f9656dcacf5d9f9443acd89d3ccf323079
Rohit Sai Anjan.D <119723402+Rohit-05002@users.noreply.github.com>
2023-08-18 12:16:03 +0530

    import java.util.*; public class num {     public static void main(String[] args)     {         int i,n;         Scanner sc = new Scanner(System.in);         n = sc.nextInt();         for (i=1;i<=n;i++)         {             if (i%2!=0)             {                 System.out.print(i +" ");             }
            }
            
        }
    }

---
+++ Odd Natural_Numbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class num
+{
+    public static void main(String[] args)
+    {
+        int i,n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (i=1;i<=n;i++)
+        {
+            if (i%2!=0)
+            {
+                System.out.print(i +" ");
+            }
+            
+        }
+        
+    }
+}
