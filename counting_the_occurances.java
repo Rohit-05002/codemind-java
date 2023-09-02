971a50f2680d1edbab236f2e17d918504f770c1a
Rohit Sai Anjan.D <119723402+Rohit-05002@users.noreply.github.com>
2023-09-02 10:11:08 +0530

    import java.util.*; public class count{     public static void main(String[]args){         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         char ch=sc.next().charAt(0);         int c=0;         for(int i=0;i<s.length();i++){             if(s.charAt(i)==ch){                 c++;             }         }         if(c>0){         System.out.print(c);         }         else{             System.out.print("-1");         }
        }
    }

---
+++ counting_the_occurances.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class count{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        char ch=sc.next().charAt(0);
+        int c=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==ch){
+                c++;
+            }
+        }
+        if(c>0){
+        System.out.print(c);
+        }
+        else{
+            System.out.print("-1");
+        }
+        
+    }
+}
