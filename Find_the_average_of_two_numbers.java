fa95756df33769334560b18ae31968abcabeac51
Rohit Sai Anjan.D <119723402+Rohit-05002@users.noreply.github.com>
2023-09-10 12:18:33 +0530

    import java.util.*; public class in{     public static void main(String[]args){         Scanner sc=new Scanner(System.in);         double a=sc.nextDouble();         double b=sc.nextDouble();         double ma=a+b;         System.out.printf("%.4f",ma/2);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class in{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextDouble();
+        double b=sc.nextDouble();
+        double ma=a+b;
+        System.out.printf("%.4f",ma/2);
+    }
+}
